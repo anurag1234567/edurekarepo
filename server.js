a4d9af648088b20ec65ab26fb304c5efc9885c7b
anurag1234567 <mailanurag15@gmail.com>
2017-05-28 18:22:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,8 +51,5 @@ app.put('/employeelist/:id', function(req,res){
 });
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
- 
+app.listen(process.env.PORT || 5000)
  
